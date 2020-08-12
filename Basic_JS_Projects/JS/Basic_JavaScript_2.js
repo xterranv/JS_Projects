088bfee509a53a189d3ca452b2e1a90b6088c954
xterranv <BeachsideTech@yahoo.com>
2020-08-12 12:23:16 -0400

    Update Basic_JavaScript_2.js

--- Basic_JS_Projects/JS/Basic_JavaScript_2.js
+++ Basic_JS_Projects/JS/Basic_JavaScript_2.js
@@ -1,9 +1,11 @@
+//change the button color to purple when clicked
 function My_First_Function() {
     var str = "The color of this is Purple";
     var result = str.fontcolor("purple");
     document.getElementById("PurpleTxt").innerHTML = result;
 }
 
+//concatenate using += 
 function myFunction() {
     var sentence = "I am learning";
     sentence += " a lot from this book.";
